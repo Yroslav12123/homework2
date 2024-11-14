eb0e3c45008a5403f0ca4bf90df9afb7cc9bbff6
Yarik <you@example.com>
2024-11-14 21:04:10 +0200

    more info about memory and strings

--- homework_py.py
+++ homework_py.py
@@ -1,2 +1,13 @@
 print("One small step for man, one giant leap for mankind.")
 
+print('☂')
+print(ord('☂'))
+
+print(chr(68))
+print(ord('D'))
+
+firstname = 'alex'
+
+print(firstname)
+
+print(id(firstname))
